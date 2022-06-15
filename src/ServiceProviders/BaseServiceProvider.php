ef7e58d8ed78a108488c2d5400e4e4a9126c6777
FusionPwN <fusionpwnu@gmail.com>
2022-06-15 16:45:10 +0100

    Update BaseServiceProvider.php

--- src/ServiceProviders/BaseServiceProvider.php
+++ src/ServiceProviders/BaseServiceProvider.php
@@ -2,20 +2,19 @@
 
 namespace Fusionpwn\ServiceProviders;
 
-use Konekt\Concord\BaseModuleServiceProvider;
 use Fusionpwn\Contracts;
 use Fusionpwn\RouteManager\CacheRouteManagerProxy;
 use Fusionpwn\Routes\NullRoute;
 use Fusionpwn\Routes\Route;
 use Fusionpwn\RouteManager\DbRouteManagerProxy;
 
-abstract class BaseServiceProvider extends BaseModuleServiceProvider
+abstract class BaseServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-	/* public function boot()
+	public function boot()
 	{
 		$this->loadMigrationsFrom(__DIR__ .
 			'resources/database/migrations');
-	} */
+	}
 
     protected function nullRoute() : void
     {
